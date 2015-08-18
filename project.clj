dfbba00c7e5537b4e1b8f3e0da9e714ac74fb1ec
gixxi <christian.meichsner@live.com>
2015-08-18 22:32:26 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0" ]
                  [org.apache/poi "3.9"]
                  [org.clojure/data.json "0.2.2"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.apache.commons/commons-vfs2 "2.0"]
-                 [compojure "1.1.5"]
-                 [hiccup "1.0.4"]
-                 [aleph "0.3.0"]
+                 [compojure "1.4.0"]
+                 [hiccup "1.0.5"]
+                 [aleph "0.4.0"]
                  [com.keminglabs/c2 "0.1.0"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.lambdaroyal/clojure-util "1.0-SNAPSHOT"]
